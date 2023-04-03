b25c28d1ca6ef3657754742795f0fb49a681b19b
Gregory Crow <gregorycrowpersonal@gmail.com>
2023-04-02 17:25:20 -0700

    working server

--- serial_server.py
+++ serial_server.py
@@ -7,10 +7,10 @@ import sys
 
 
 class TftpServer:
-    OPCODE_READ = b"1"
+    OPCODE_READ = b"\x00\x01"
     OPCODE_WRITE = b"10"
-    OPCODE_DATA = b"11"
-    OPCODE_ACK = b"100"
+    OPCODE_DATA = b"\x00\x03"
+    OPCODE_ACK = b"\x00\x04"
     OPCODE_ERR = b"101"
 
     STATE_WAITING = 0
@@ -21,6 +21,16 @@ class TftpServer:
     tftp_block_size = 512
     write_no_ack_retry = 3
     err_str = ""
+    file = None
+
+
+    def __init__(self, file):
+        self.file = file
+
+
+    def __del__(self):
+        self.file.close()
+
 
     def get_opcode(self, input_string):
         """Helper function to get the opcode from an input string.
@@ -32,24 +42,35 @@ class TftpServer:
 
         return input_string[0:2]
 
-    def wait_for_valid_ack(self, server, num_retries, block_counter):
-        for _ in range(num_retries):
-            acq = server.read(4)
-            self.get_opcode(acq)
-            block_id = acq[2:]
 
-            if block_id == block_counter:
+    def parse_ack(self, given, expected_block):
+        valid_ack = given[0] == self.OPCODE_ACK
+        correct_block = given[1] == int(expected_block).to_bytes(2, 'big')
+
+        if valid_ack and correct_block:
+            return True
+        else:
+            print(
+                f"Expected opcode of {self.OPCODE_ACK}. Got opcode of {given[0]}"
+            )
+            print(
+                f"Expected block counter of {expected_block}. Got block counter of {given[1]}"
+            )
+
+
+    def wait_for_valid_ack(self, server, num_retries, block_counter, first_attempt):
+        attempt = first_attempt
+        for _ in range(num_retries):
+            if self.parse_ack((self.get_opcode(attempt), attempt[2:4]), block_counter):
+                print(f"Valid acq on block {block_counter}")
                 return True
-            else:
-                print(
-                    f"Expecting block acq on block id {block_counter}."
-                    f"Instead got {block_id}"
-                )
+
+            attempt = server.read(4)
 
         # If we got here, then we failed to acq the block.
         return False
 
-    def run_sm(self, server, file):
+    def run_sm(self, server):
         current_state = self.STATE_WAITING
 
         while(1):
@@ -61,13 +82,13 @@ class TftpServer:
                 server.timeout = None
 
                 # Wait on the opcode to dictate what we should do here.
-                server_request = server.read()
+                server_request = server.read(2)
                 opcode = self.get_opcode(server_request)
 
                 # This is a read request. As the server, we should transfer to
                 # the writing state to fulfill the client's read request.
                 if opcode == self.OPCODE_READ:
-                    print(f"Recieved read request for {file}!")
+                    print(f"Recieved read request for {self.file}!")
                     current_state = self.STATE_WRITING
 
                 # This is a write request. As the server, we should never
@@ -84,15 +105,11 @@ class TftpServer:
 
                 # Throw and error if have an expect opcode for this state.
                 else:
-                    print(
-                        f"Got an unexpected opcode of {opcode} in the waiting"
-                        " state. Throwing err :("
-                    )
-                    self.err_str = (
-                        f"Requested opcode {opcode} is not a valid opcode in"
-                        " the waiting state."
-                    )
-                    current_state = self.STATE_ERR
+                    server.timeout = 1
+                    server_request += server.read_until('\n')
+                    server.timeout = None
+
+                    print(f"{server_request}")
 
             # The writing state. Send the desired file to out the serial
             # server.
@@ -101,6 +118,7 @@ class TftpServer:
                 # how long we will wait for an acq for each data block.
                 server.timeout = 2
                 block_counter = 0
+                print("Starting write")
 
                 # If the last read size of the file is less than the tftp
                 # block size, we are at the end of the file and now can move
@@ -109,20 +127,30 @@ class TftpServer:
                 while last_read_block_size == self.tftp_block_size:
                     # Frame our message with the correct opcode and block
                     # counter.
-                    message = str(self.OPCODE_DATA)
-                    message += str(block_counter)
+                    print("sending message")
+                    message = self.OPCODE_DATA
+                    message += block_counter.to_bytes(2, 'big')
 
-                    # Read the request amount of the file and add it the
-                    # message to be sent.
-                    read_block = file.read(self.tftp_block_size)
-                    message += read_block
+                    read_block = self.file.read(self.tftp_block_size)
                     last_read_block_size = len(read_block)
+
+                    message += last_read_block_size.to_bytes(2, 'big')
+                    message += read_block
+
+                    # Print out the message we want to send in hex
+                    print(''.join('{:02x} '.format(x) for x in message))
 
                     # Write our message.
                     server.write(message)
 
+                    acq = server.read(4)
+                    # rply = server.read(last_read_block_size)
+
+                    # print("recived this from the client")
+                    # print(''.join('{:02x} '.format(x) for x in rply))
+
                     if self.wait_for_valid_ack(
-                        server, self.write_no_ack_retry, block_counter
+                        server, self.write_no_ack_retry, block_counter, acq
                     ):
                         block_counter += 1
                     else:
@@ -153,7 +181,6 @@ class TftpServer:
 def main(*args):
     usb_device_name = "/dev/tty.usbmodem2103"
     serial_baud_rate = 115200
-    tftp_server = TftpServer()
     try:
         server = serial.Serial(usb_device_name, serial_baud_rate)
 
@@ -165,13 +192,13 @@ def main(*args):
         print("Starting TFTP server!")
         print(f"Opening file {args[0][0]}")
 
-        f = open(args[0][0], "r")
-
+        f = open(args[0][0], "rb")
+        tftp_server = TftpServer(f)
 
         if f.closed:
             print("file failed to open")
         else:
-            tftp_server.run_sm(server, f)
+            tftp_server.run_sm(server)
 
     except:
         server.close()
